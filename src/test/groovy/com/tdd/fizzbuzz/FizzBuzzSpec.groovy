624d9f40f8c0952a1dc501b270248b1e82c54e86
KKV Git <kkv4all@outlook.com>
2019-10-05 18:37:51 +0530

    Fizzbuzz Kata - Initial File Setup

---
+++ src/test/groovy/com/tdd/fizzbuzz/FizzBuzzSpec.groovy
@@ -0,0 +1,18 @@
+package com.tdd.fizzbuzz;
+
+/**
+ * @author kkv4all
+ * @description
+ * Problem Definition: A program that prints the numbers from 1 to 100.
+ * But for multiples of three print “Fizz” instead of the number
+ * and for the multiples of five print “Buzz”.
+ * For numbers which are multiples of both three and five print “FizzBuzz “.
+ *
+ * URL: http://codingdojo.org/kata/FizzBuzz/
+ */
+
+import spock.lang.Specification
+
+public class FizzBuzzSpec extends Specification{
+
+}
---
+++ src/test/java/com/tdd/fizzbuzz/FizzBuzzShould.java
@@ -0,0 +1,30 @@
+package com.tdd.fizzbuzz;
+
+/**
+ * @author kkv4all
+ * @description
+ * Problem Definition: A program that prints the numbers from 1 to 100. 
+ * But for multiples of three print “Fizz” instead of the number 
+ * and for the multiples of five print “Buzz”. 
+ * For numbers which are multiples of both three and five print “FizzBuzz “.
+ * 
+ * URL: http://codingdojo.org/kata/FizzBuzz/
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzShould {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
