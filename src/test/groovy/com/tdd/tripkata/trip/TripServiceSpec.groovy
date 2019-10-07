f20e114d685b50d8f19b55023f9f998818d2c2af
KKV Git <kkv4all@outlook.com>
2019-10-08 00:44:15 +0530

    Trip Service Refactor Kata - Added First Test to check Guest users but found RuntimeException(Not expected)

--- src/test/groovy/com/tdd/tripkata/trip/TripServiceSpec.groovy
+++ src/test/groovy/com/tdd/tripkata/trip/TripServiceSpec.groovy
@@ -1,14 +1,24 @@
 package com.tdd.tripkata.trip
 
+import com.tdd.tripkata.user.User
+
 import spock.lang.Specification
 
 class TripServiceSpec extends Specification {
+	
+	private static final User GUEST = null;
 	
 	def setup() {
 		
 	}
 	
-	def test () {
+	def "throw Exception if user not LoggedIn" () {
+		given:""
+		TripService tripService = new TripService();
 		
+		when:
+			tripService.getTripsByUser(null);
+		then: "UserNotLoggedInException is thorwn"
+			thrown(UserNotLoggedInException.class)
 	}
 }
--- src/test/java/com/tdd/tripkata/trip/TripServiceShould.java
+++ src/test/java/com/tdd/tripkata/trip/TripServiceShould.java
@@ -5,15 +5,23 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.tdd.tripkata.exception.UserNotLoggedInException;
+import com.tdd.tripkata.user.User;
+
 public class TripServiceShould {
+
+	private static final User GUEST = null;
 
 	@Before
 	public void setUp() throws Exception {
 	}
 
-	@Test
-	public void testGetTripsByUser() {
-		fail("Not yet implemented");
+	@Test(expected = UserNotLoggedInException.class)
+	public void throwExceptionifNotLoggedIn() {
+		TripService tripService = new TripService();
+		
+		
+		tripService.getTripsByUser(null);
 	}
 
 }
