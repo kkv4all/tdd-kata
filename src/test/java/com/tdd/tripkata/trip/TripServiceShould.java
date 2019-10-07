028fc1dd015c6dd5fa80a4240e4c91da01cc501e
KKV Git <kkv4all@outlook.com>
2019-10-08 01:20:37 +0530

    Trip Service Refactor Kata - Scenario 2: Return No Trip if User is not friend with logged-in user

--- src/test/java/com/tdd/tripkata/trip/TripServiceShould.java
+++ src/test/java/com/tdd/tripkata/trip/TripServiceShould.java
@@ -1,6 +1,9 @@
 package com.tdd.tripkata.trip;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,20 +13,45 @@ import com.tdd.tripkata.user.User;
 
 public class TripServiceShould {
 
-	private static final User GUEST = null;
+	private TripService tripService;
+
 	private User loggedInUser;
+	private static final User GUEST = null;
+	private static final User ANOTHER_USER = new User();
+	private static final User REGISTERED_USER = new User();
+	
+	private static final Trip UDAYPUR = new Trip();
+	private static final Trip SIMLA = new Trip();
+	
+	private static final List<Trip> NO_TRIP = new ArrayList<Trip>();
+
 
 	@Before
 	public void setUp() throws Exception {
+		tripService = new TripServiceSeam();
 	}
 
 	@Test(expected = UserNotLoggedInException.class)
 	public void throwExceptionifNotLoggedIn() {
-		TripService tripService = new TripServiceSeam();
 		loggedInUser=GUEST;
 		
 		tripService.getTripsByUser(null);
 	}
+	
+	@Test
+	public void returnNoTripIfUserIsNotFriend() throws Exception {
+		User notFriend = new User();
+		notFriend.addFriend(ANOTHER_USER);
+		notFriend.addTrip(UDAYPUR);
+		notFriend.addTrip(SIMLA);
+		
+		loggedInUser = REGISTERED_USER;
+		
+		List<Trip> list = tripService.getTripsByUser(notFriend);
+		
+		assertEquals(NO_TRIP, list);
+	}
+	
 
 	class TripServiceSeam extends TripService {
 		protected User getUserSession() {
