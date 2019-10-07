d651ea253317c2d281776bbdeab10fd383630147
KKV Git <kkv4all@outlook.com>
2019-10-08 03:46:19 +0530

    Problem Defination for Bank Kata

--- src/integrationtest/java/com/tdd/bankkata/StatementPrintingWorkflow.java
+++ src/integrationtest/java/com/tdd/bankkata/StatementPrintingWorkflow.java
@@ -1,5 +1,21 @@
 package com.tdd.bankkata;
 
+/**
+ ** @author kkv4all
+ * @description Problem description - Bank kata
+ *   Create a simple bank application with the following features:
+ *    - Deposit into Account
+ *    - Withdraw from an Account
+ *    - Print a bank statement to the console
+ *    
+ *   Acceptance criteria
+ *   Statement should have transactions in the following format:
+ *   DATE       | AMOUNT  | BALANCE
+ *   10/04/2014 | 500.00  | 1400.00
+ *   02/04/2014 | -100.00 | 900.00
+ *   01/04/2014 | 1000.00 | 1000.00
+ */
+
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
