ea86cd617113640959260032c7f6e861dec7578f
KKV Git <kkv4all@outlook.com>
2019-10-08 00:52:49 +0530

    Trip Service Refactor Kata - Solved unexpected exception due to static method call in UserSession using TEST SEAM

--- src/test/groovy/com/tdd/tripkata/trip/TripServiceSpec.groovy
+++ src/test/groovy/com/tdd/tripkata/trip/TripServiceSpec.groovy
@@ -1,5 +1,6 @@
 package com.tdd.tripkata.trip
 
+import com.tdd.tripkata.exception.UserNotLoggedInException
 import com.tdd.tripkata.user.User
 
 import spock.lang.Specification
@@ -7,6 +8,7 @@ import spock.lang.Specification
 class TripServiceSpec extends Specification {
 	
 	private static final User GUEST = null;
+	private User loggedInUser;
 	
 	def setup() {
 		
@@ -14,11 +16,17 @@ class TripServiceSpec extends Specification {
 	
 	def "throw Exception if user not LoggedIn" () {
 		given:""
-		TripService tripService = new TripService();
-		
+			TripService tripService = new TripServiceSeam();
+			loggedInUser=GUEST;
 		when:
 			tripService.getTripsByUser(null);
 		then: "UserNotLoggedInException is thorwn"
 			thrown(UserNotLoggedInException.class)
+	}
+	
+	class TripServiceSeam extends TripService {
+		protected User getUserSession() {
+			return loggedInUser;
+		}
 	}
 }
--- src/test/java/com/tdd/tripkata/trip/TripServiceShould.java
+++ src/test/java/com/tdd/tripkata/trip/TripServiceShould.java
@@ -11,6 +11,7 @@ import com.tdd.tripkata.user.User;
 public class TripServiceShould {
 
 	private static final User GUEST = null;
+	private User loggedInUser;
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,10 +19,15 @@ public class TripServiceShould {
 
 	@Test(expected = UserNotLoggedInException.class)
 	public void throwExceptionifNotLoggedIn() {
-		TripService tripService = new TripService();
-		
+		TripService tripService = new TripServiceSeam();
+		loggedInUser=GUEST;
 		
 		tripService.getTripsByUser(null);
 	}
 
+	class TripServiceSeam extends TripService {
+		protected User getUserSession() {
+			return loggedInUser;
+		}
+	}
 }
