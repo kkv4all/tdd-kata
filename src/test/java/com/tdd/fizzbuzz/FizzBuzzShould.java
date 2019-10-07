b63c87c25a54179e1277b4c726815cf0242cdbfc
KKV Git <kkv4all@outlook.com>
2019-10-08 03:28:06 +0530

    Fixed gradle warning due to some special character

--- src/test/java/com/tdd/fizzbuzz/FizzBuzzShould.java
+++ src/test/java/com/tdd/fizzbuzz/FizzBuzzShould.java
@@ -4,9 +4,9 @@ package com.tdd.fizzbuzz;
  * @author kkv4all
  * @description
  * Problem Definition: A program takes a Number as input. 
- * For multiples of three print “Fizz” instead of the number 
- * and for the multiples of five print “Buzz”. 
- * For numbers which are multiples of both three and five print “FizzBuzz “.
+ * For multiples of three print "Fizz" instead of the number 
+ * and for the multiples of five print "Buzz". 
+ * For numbers which are multiples of both three and five print "FizzBuzz".
  * For rest of the number it should return the same number back.
  */
 
